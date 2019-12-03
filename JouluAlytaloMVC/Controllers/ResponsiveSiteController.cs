a568cf372da38bc885bd70b29bf56588f07ca03f
Jarkko Haikarainen <jarkko.haikarainen@student.careeria.fi>
2019-12-03 12:41:39 +0200

    Lisätty CSS määrityksiä

---
+++ JouluAlytaloMVC/Controllers/ResponsiveSiteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JouluAlytaloMVC.Controllers
+{
+    public class ResponsiveSiteController : Controller
+    {
+        // GET: ResponsiveSite
+        public ActionResult AlySite()
+        {
+            return View();
+        }
+    }
+}
